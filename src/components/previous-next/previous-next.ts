69e957deec24f75c83d9171e460297e99eaaa062
sirarthurnell <sirarthurnell@gmail.com>
2018-09-15 15:01:20 +0200

    New previous next component added

---
+++ src/components/previous-next/previous-next.ts
@@ -0,0 +1,28 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+/**
+ * Represents a previous next kind of component.
+ */
+@Component({
+  selector: 'previous-next',
+  templateUrl: 'previous-next.html'
+})
+export class PreviousNextComponent {
+  @Input() text = '';
+  @Output() previous = new EventEmitter();
+  @Output() next = new EventEmitter();
+
+  /**
+   * Emits the previous event.
+   */
+  emitPrevious(): void {
+    this.previous.emit();
+  }
+
+  /**
+   * Emits the next event.
+   */
+  emitNext(): void {
+    this.next.emit();
+  }
+}
--- src/pages/year/year.module.ts
+++ src/pages/year/year.module.ts
@@ -1,13 +1,10 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { YearPage } from './year';
+import { ComponentsModule } from '../../components/components.module';
 
 @NgModule({
-  declarations: [
-    YearPage,
-  ],
-  imports: [
-    IonicPageModule.forChild(YearPage),
-  ],
+  declarations: [YearPage],
+  imports: [IonicPageModule.forChild(YearPage), ComponentsModule]
 })
 export class YearPageModule {}
