e20e6cd138a39484f32c268db0f860706f3c0aaa
Arturo Medina <sirarthurnell@gmail.com>
2018-09-21 13:14:20 +0200

    DayFormatter connected to formatter factory

--- src/models/formatters/formatter-factory.ts
+++ src/models/formatters/formatter-factory.ts
@@ -1,10 +1,22 @@
-import { Formatter } from "./formatter";
+import { Day } from '../time/day';
+import { DayFormatter } from './day-formatter';
+import { Formatter } from './formatter';
 
 /**
  * Factory for the different formatters.
  */
 export class FormatterFactory {
+
+  /**
+   * Provides a formatter to format the
+   * specified entity.
+   * @param entity Entity to format.
+   */
   createFormatter(entity: any): Formatter {
-    return null;
+    if (entity instanceof Day) {
+      return new DayFormatter(entity as Day);
+    } else {
+      return null;
+    }
   }
 }
